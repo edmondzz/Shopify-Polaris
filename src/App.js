5e6076dbc0050325732e38d3a6b7cb52eb0c77c4
edmond saliji <edmondsalijii@gmail.com>
2023-03-10 12:28:52 +0100

    Importing useAuth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../src/Auth/useAuth';
 import AuthForm from './Auth/AuthForm';
 import Dashboard from './Auth/Dashboard';
 import './App.css';
+
 function App() {
   const { isAuthenticated } = useAuth(); // get the isAuthenticated value from the useAuth hook
 
